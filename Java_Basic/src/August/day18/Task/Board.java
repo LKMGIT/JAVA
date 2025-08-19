8d1eb4bfba20d50361efee418bd23b66c95df500
LKM <leekmin1804@gmail.com>
2025-08-19 17:52:01 +0900

    board menu

--- Java_Basic/src/August/day18/Task/Board.java
+++ Java_Basic/src/August/day18/Task/Board.java
@@ -1,8 +1,15 @@
 package August.day18.Task;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class Board {
     private int bno;
     private String btitle;
-
+    private String bcontent;
+    private String bwriter;
+    private Date bdate;
 
 }
--- Java_Basic/src/August/day18/Task/BoardExample.java
+++ Java_Basic/src/August/day18/Task/BoardExample.java
@@ -1,4 +1,32 @@
 package August.day18.Task;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardExample {
+    public static void main(String[] args) {
+     List<Board> boards = new ArrayList<Board>();
+
+    }
+    
+    public void list(Board[] boards) {
+        System.out.println("""
+                [게시물 목록]
+                --------------------------------------------------
+                no     writer      date         title
+                --------------------------------------------------""");
+        for(Board b : boards){
+            System.out.println(b.getBno() + "    " + b.getBwriter() +
+                    "    " + b.getBdate() + "    " + b.getBtitle());
+        }
+        System.out.println("");
+        System.out.println("------------------------------------------");
+        mainMenu();
+    }
+
+    public void mainMenu() {
+        System.out.println("""
+                메인 메뉴 : 1. Create | 2. Read | 3. Clear | 4. Exit
+                메누 선택 : """);
+    }
 }
